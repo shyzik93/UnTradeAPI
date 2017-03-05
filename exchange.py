392b4da678ce7f6946075a078a7a83bfe646651a
Konstantin Polyakov <kostyan_93@mail.ru>
2017-03-06 01:01:09 +0300

    you can create and cancel orders for exmo

--- exchange.py
+++ exchange.py
@@ -2,7 +2,7 @@
 # Author: Konstantin Polyakov
 # Date: 25.07.2014, 18.08.2015, 06.11.2015, 18.06.2016
 
-import json, hmac, hashlib, os, time
+import json, hmac, hashlib, os, time, os
 from urllib import request, parse
 
 import requests
@@ -16,11 +16,10 @@ class Doer:
 
 # Класс  протоАПИ бирж
 
-class proAPI:
+class ProAPI:
     def __init__(self, conf={}):
       self.conf = conf
       self.do = Doer(self)
-
     
     #self.opener = request.build_opener(request.HTTPCookieProcessor(cj))
 
@@ -40,11 +39,29 @@ class proAPI:
         sign = hmac.new(bytearray(self.conf['secret'], 'utf-8'), msg=data, digestmod=hashlib.sha512).hexdigest()
         return data, sign
 
+    def get_nonce(self, filename):
+        file_nonce = os.path.join(os.path.dirname(os.path.abspath('')), filename+'.txt')
+
+        if not os.path.exists(file_nonce): nonce = '1'
+        else:
+            f = open(file_nonce, 'r')
+            nonce = f.read()
+            f.close()
+
+        if nonce=='': nonce = '1'
+
+        f = open(file_nonce, 'w')
+        f.write(str(int(nonce)+1))
+        f.close()
+
+        return nonce
+
+
 # Класс цены
 
-class Price():
-    def __init__(self, pair, buy, sell):
-        self.pair = pair
+class Price:
+    def __init__(self, upair, buy, sell):
+        self.upair = upair
         self.buy = float(buy)
         self.sell = float(sell)
         self.calc_base_values()
@@ -54,32 +71,72 @@ class Price():
         self.spread = self.buy - self.sell # спрэд
         self.mean = (self.buy + self.sell) / 2 # среднее арифметическое
 
+class Order:
+    def __init__(self, upair, action, count, price):
+        self.upair = upair
+        self.action = action
+        self.count = count
+        self.price = price
+
+        self.order_id = None
+
+    def setId(self, order_id):
+        self.order_id = order_id
+
 # Классы АПИ бирж
 
-class exchange_exmo(proAPI):
+class exchange_exmo(ProAPI):
     exmo_url = "https://api.exmo.com/v1/"
 
     #def sign(self, data):
     #    return hmac.new(key=bytearray(self.conf['secret'], 'utf-8'), msg=data, digestmod=hashlib.sha512).hexdigest()
 
+    def __init__(self, conf):
+        ProAPI.__init__(self, conf)
+
+        #pair_settings, success, errs = self.do.pair_settings()
+        #if success:
+        #    self.pairs = list(pair_settings.keys())
+        #else:
+        #    self.pairs = {}
+        #    print('###', 'Ошибка получения списка валютных пар: ', errs)
+
+
+        #print('-'*50)
+        #print(self.pairs, success, errs)
+        #print('-'*50)
+
     def shell(self, api_name, api_params):
         if api_name.startswith('_'): # Auth API
             api_name = api_name[1:]
-            api_params["nonce"] = str(time.time()).replace('.', '')#split('.')[0]
-            #sign = self.sign(bytearray(parse.urlencode(api_params), 'utf-8'))
+            api_params["nonce"] = int(round(time.time()*1000))#self.get_nonce('exmo_nonce')#str(time.time()).replace('.', '')#split('.')[0]
             api_params, sign = self.sign(api_params)
-            header = {"Key": self.conf['key'], "Sign":sign}
+            header = {"Key": self.conf['key'], "Sign":sign, "Content-type": "application/x-www-form-urlencoded"}
             data, success, errors = self.urlopen(self.exmo_url + api_name, POST=api_params, headers=header)
         else: # Public API
             data, success, errors = self.urlopen(self.exmo_url + api_name, GET=api_params)
 
         if success:
             data = json.loads(data)
-            return (data, True, errors) if 'error' not in data else (None, False, errors+[data])
+            return (data, True, errors) #if 'error' not in data else (None, False, errors+[data])
         else: return None, False, errors
 
-    def price(self, upair):
+    '''def upair2pair(self, upair, to_reverse=False):
         pair = upair.replace('-', '_').upper()
+        return pair'''
+
+    def upair2pair(self, upair, to_reverse=False):
+        pair = upair.split('-')
+        if to_reverse: pair.reverse()
+        return '_'.join(pair).upper()
+
+    '''def reverse_pair(self, pair):
+        pair = pair.split('_')
+        pair.reverse()
+        return '_'.join(pair)'''
+
+    def price(self, upair):
+        pair = self.upair2pair(upair)
 
         data, success, errors = self.do.order_book(pair=pair, limit=0)
         if success:
@@ -97,19 +154,52 @@ class exchange_exmo(proAPI):
 
         return price, success, errors
 
-    def new_order(self, pair, action, quantity, price):
+    '''def order(self, upair, data1, data2=None):
+        data2 = {} if data2 is None else data2
+
+        pair = self.upair2pair(upair)
+        is_reversed = False
+        if pair not in self.pairs:
+            pair = self.reverse_pair(pair)
+            is_reversed = True
+
+        return pair, '', '';'''
+
+    
+    '''def order(self, buy, sell):
+        upair = buy['name'] + '-' + sell['name']
+
+        pair = self.upair2pair(upair)
+        is_reversed = False
+        if pair not in self.pairs:
+            pair = self.upair2pair(upair, True)
+            is_reversed = True
+
+        return pair, '', '';'''
+
+    def order(self, pair, action, count, price):
         pair = pair.replace('-', '_').upper()
         if price == 'market':
             price = 0
-            action = market +'_'+ action
-        data, success, errors = self.do._order_create(pair=pair, quantity=quantity, price=price, type=action)
-        return data, success, errors
+            action = 'market_'+ action
+        data, success, errors = self.do._order_create(pair=pair, type=action, price=price, quantity=count)
+
+        order = Order(pair, action, count, price)
+
+        if success:
+            if data['result']:
+                order.setId(data['order_id'])
+            else: errors.append(data['error'])
+
+        return order, success, errors
 
     def cancel_order(self, order_id):
         data, success, errors = self.do._order_cancel(order_id=order_id)
-        return data, success, errors
+        if success:
+            if not data['result']: errors.append(data['error'])
+        return None, success, errors
 
-class exchange_btce(proAPI):
+class exchange_btce(ProAPI):
     btce_url = "https://btc-e.nz/tapi"
     btce_url2 = "https://btc-e.nz/api/3/%s/%s"
     __wait_for_nonce = False # for btce
@@ -118,7 +208,7 @@ class exchange_btce(proAPI):
         if self.__wait_for_nonce: time.sleep(1)
         if api_name.startswith('_'): # Auth API
             api_name = api_name[1:]
-            nonce_v = str(time.time()).split('.')[0]
+            nonce_v = str(time.time()).split('.')[0] #int(round(time.time()*1000))
             api_params['method'] = api_name
             api_params['nonce'] = nonce_v
             #post_data = bytearray(parse.urlencode(api_params), 'utf-8')
@@ -155,14 +245,19 @@ class exchange_btce(proAPI):
 
         return price, success, errors
 
+    def new_order(self, pair, action, count, price):
+        pair = pair.replace('-', '_').upper()
+        data, success, errors = self.do._Trade(pair=pair, type=action, rate='', amount=count)
+        return data, success, errors
+
     def new_order(self, pair, action):
-        pass
+        self.do._Trade(pair='', type='', rate='', amount='')
 
     def cancel_order(self, order_id):
         data, success, errors = self.do._CancelOrder(order_id=order_id)
         return data, success, errors
 
-class exchange_poloniex(proAPI):
+class exchange_poloniex(ProAPI):
     trade_url =  'https://poloniex.com/tradingApi'
     public_url = 'https://poloniex.com/public'
 
@@ -203,7 +298,7 @@ class exchange_poloniex(proAPI):
 
         return price, success, errors
 
-    def new_order(self, pair, action):
+    def new_order(self, pair, action, count, price):
         pass
 
     def cancel_order(self, order_id):
@@ -214,91 +309,8 @@ class exchange_poloniex(proAPI):
 
 # ---------------------- Старый вариант -----------
 class API():
-  exmo_url = "https://api.exmo.com/v1/"
-  btce_url = "https://btc-e.com/tapi"
-  btce_url2 = "https://btc-e.com/api/3/%s/%s"
-  __wait_for_nonce = False
-
-  def __init__(self, conf):
-    self.conf = conf
-    self.exmo_it = request.build_opener()#internettools.InternetTools(geterrors=True)
-    self.btce_it = request.build_opener()#internettools.InternetTools(geterrors=True)
-    self.opener = request.build_opener()
-
-  def __getattr__(self, api_name, *args):
-    return lambda **args: self.shell(api_name, args)
-
-  def urlopen(self, url, POST=None, GET=None, headers={}):
-    if isinstance(POST, dict): POST = parse.urlencode(POST)
-    if isinstance(GET, dict): GET = parse.urlencode(GET)
-    if GET != None: url += '?' + GET
-
-    self.opener.addheaders.extend(headers.items())
-    try: response = self.opener.open(url, POST)
-    except Exception as e: return e.reason, None, None
-    str_page = response.read()
-
-    return None, str_page, response
-
-  def exmo_shell(self, api_name, api_params):
-    if api_name.startswith('_'): # Auth API
-      api_name = api_name[1:]
-      api_params["nonce"] = str(time.time()).replace('.', '')#split('.')[0]
-      post_data = bytearray(parse.urlencode(api_params), 'utf-8')
-      sign = hmac.new(key=bytearray(self.conf['exmo']['secret'], 'utf-8'), msg=post_data, digestmod=hashlib.sha512).hexdigest()
-      header = {"Key": self.conf['exmo']['key'], "Sign":sign}
-      #print header
-      answer = self.urlopen(self.exmo_url + api_name, POST=post_data, headers=header)
-    else: # Public API
-      answer = self.urlopen(self.exmo_url + api_name, GET=api_params, headers={'Accept-Charset': 'utf-8' })
-    if answer[0] == None: return True, json.loads(str(answer[1], 'utf-8'))
-    else: return False, answer[0]
-
-  def btce_shell(self, api_name, api_params):
-    if self.__wait_for_nonce: time.sleep(1)
-    if api_name[0] == '_': # Auth API
-      api_name = api_name[1:]
-      nonce_v = str(time.time()).split('.')[0]
-      api_params['method'] = api_name
-      api_params['nonce'] = nonce_v
-      post_data = bytearray(parse.urlencode(api_params), 'utf-8')
-      sign = hmac.new(bytearray(self.conf['btce']['secret'], 'utf-8'), post_data, digestmod=hashlib.sha512).hexdigest()
-      headers = {"Content-type" : "application/x-www-form-urlencoded",
-                     "Key" : self.conf['btce']['key'],
-                     "Sign" : sign}
-      answer = self.urlopen(self.btce_url, POST=post_data, headers=headers)
-    else: # Public API
-      answer = self.urlopen(self.btce_url2 % (api_name, api_params['pairs']))
-    if answer[0] == None: return True, json.loads(str(answer[1], 'utf-8'))
-    else: return False, answer[0]
-
-  def shell(self, api_name, api_params):
-    print("---------")
-    #if api_name == 'exmo': return 
-    #return self.__getattribute__(name+'_shell')(api_name, api_params)
-    name, api_name = api_name.split('_', 1)
-    return self.__getattribute__(name+'_shell')(api_name, api_params)
 
   '''# ########## Высоуровневые функции ############
-  def normalize_pair(self, ec_name, pair):
-    if ec_name=='exmo': return pair.upper()
-    elif ec_name=='btce': return pair.replace('rub', 'rur')
-
-  def denormalize_pair(self, ec_name, pair):
-    if ec_name=='exmo': return pair.lower()
-    elif ec_name=='btce': return pair.replace('rur', 'rub')
-
-  def Price(self, ec_name, pair, _type):
-    ''' '''Пары пишутся маленькими буквами. Рубль - это rub. ''' '''
-    pair = self.normalize_pair(ec_name, pair)
-    if ec_name == 'exmo':
-      if _type=='buy': _type = 'sell'
-      elif _type=='sell': _type = 'buy'
-      answer = self.exmo_ticker()
-      return float(answer[pair][_type+'_price'])
-    elif ec_name == 'btce':
-      answer = self.btce_ticker(pairs=pair)
-      return float(answer[pair][_type])
 
   def PairsList(self, ec_name):
     if ec_name == 'exmo': pairs = self.exmo_ticker().keys()
@@ -323,7 +335,7 @@ if __name__ == '__main__':
 
     # Тест бирж
 
-    import configparser, os
+    import configparser
     config = configparser.ConfigParser()
     path_conf = os.path.join(os.path.dirname(os.path.abspath('')), 'conf_exchange.txt')
     config.read(path_conf)
@@ -332,35 +344,57 @@ if __name__ == '__main__':
     btce = exchange_btce(config['btce'])
     polo = exchange_poloniex()
 
-    #print()
-    print(exmo.do._user_info())
-    #print()
-    #print(btce.do.info(pairs=[]))
-    #print(btce.do._getInfo())
-    print('\n\n')
-
-    price, success, errs = exmo.price('btc-usd')
-    print(price.buy, price.sell, price.spread)
-
-    price, success, errs = btce.price('btc-usd')
-    print(price.buy, price.sell, price.spread)
-
-    price, success, errs = polo.price('usdt-btc') # usd = dollar, usdt = teather dollar
-    print(price.buy, price.sell, price.spread)
-
-    #data, success, errs = polo.do.returnTicker()
-    #print(data, success, errs)
-
     def fprice(price_float):
         return '{0:<15}'.format(price_float)
+
+    print(exmo.do._user_info())
+    #print(polo.do.(pairs=[]))
+    print(btce.do._getInfo())
+
+    '''----------------------------------------------------------
+    -- Монитор --------------------------------------------------
+    ----------------------------------------------------------'''
+
+    print('-'*60)
+    print('     | ', fprice('BUY'), fprice('SELL'), fprice('SPREAD'))
+    print('-'*60)
+
+    price, success, errs = exmo.price('btc-usd')
+    print('EXMO | ', fprice(price.buy), fprice(price.sell), fprice(price.spread))
+
+    price, success, errs = btce.price('btc-usd')
+    print('BTCE | ', fprice(price.buy), fprice(price.sell), fprice(price.spread))
+
+    price, success, errs = polo.price('usdt-btc') # usd = dollar, usdt = teather dollar
+    print('POLO | ', fprice(price.buy), fprice(price.sell), fprice(price.spread))
+
+    print('-'*60)
+
+    '''----------------------------------------------------------
+    -- Ордера ---------------------------------------------------
+    ----------------------------------------------------------'''
+
+
+    #data, success, errs = exmo.order({'name': 'btc', 'count':10, 'price':'market'}, {'name':'usd'})
+    order, success, errs = exmo.order('ltc-rub', 'buy', 0.1, 210)
+    print('\n', order, success, errs)
+    if success:
+        data, success, errs = exmo.cancel_order(order.order_id)
+        print('\n', data, success, errs)
+    #data, success, errs = exmo.order({'name': 'usd'}, {'name':'btc', 'count':10, 'price':'market'})
+    #print('', data, success, errs)
+
 
     threshold = { # пороговые значения
         'eth-rub': {'max_buy':759, 'max_sell':750}
     }
 
-    while True:
+    #while True:
 
-        '''price, success, errs = exmo.price('eth-rub')
+    #data, success, errs = polo.do.returnTicker()
+    #print(data, success, errs)
+
+    '''price, success, errs = exmo.price('eth-rub')
         if success:
             print('EXMO:', fprice(price.buy), price.sell)
             if (price.sell > threshold['eth-rub']['max_sell']): print('MAX SELL PRICE IS OBTAINED')
@@ -371,16 +405,16 @@ if __name__ == '__main__':
             print('BTCE:', fprice(price.buy), price.sell)
         else: print('BTCE: ERROR:', errs)'''
 
-        price, success, errs = polo.price('zec-usdt')
+    '''price, success, errs = polo.price('usdt-zec')
         if success:
-            print('ZCAH -> USD:', fprice(price.buy), price.sell)
-        else: print('ZCAH -> USD: ERROR:', errs)
+            print('ZEC -> USD:', fprice(price.buy), price.sell)
+        else: print('ZEC -> USD: ERROR:', errs)
 
-        price, success, errs = polo.price('zec-btc')
+        price, success, errs = polo.price('btc-zec')
         if success:
-            print('ZCAH -> BTC:', fprice(price.buy), price.sell)
-        else: print('ZCAH -> BTC: ERROR:', errs)
+            print('ZEC -> BTC:', fprice(price.buy), price.sell)
+        else: print('ZEC -> BTC: ERROR:', errs)
 
         print()
 
-        time.sleep(30)
+        time.sleep(30)'''
